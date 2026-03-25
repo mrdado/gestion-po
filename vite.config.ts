a9252a37ed041b7f0b6fcb74730c12609014000e
ESSAIS <essais@company.com>
2026-03-25 12:05:18 +0100

    feat: add PWA support to allow standalone app installation

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'logo.png'],
+      manifest: {
+        name: 'Gestion PO',
+        short_name: 'GestionPO',
+        description: 'Application de Gestion de PO',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
